69b1660b6b286c0da46e5e49f7cab7a0afd42c02
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-01-05 13:42:44 +0100

    add changing language in html

--- src/utils/changeLanguage.tsx
+++ src/utils/changeLanguage.tsx
@@ -1,7 +1,14 @@
 export function changeLanguage(language: string) {
+  const htmlElement = document.getElementById("html");
+  if (htmlElement) {
+    htmlElement.setAttribute("lang", "cz");
+  }
   if (language === "en") {
     return "cz";
   } else {
+    if (htmlElement) {
+      htmlElement.setAttribute("lang", "en");
+    }
     return "en";
   }
 }
